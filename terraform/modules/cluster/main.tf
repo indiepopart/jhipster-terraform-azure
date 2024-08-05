ce33f5577473fe262e99b2b9f41590344a770956
indiepopart <jimena@gmail.com>
2024-08-05 12:53:55 -0300

    remove optionals from cluster

--- terraform/modules/cluster/main.tf
+++ terraform/modules/cluster/main.tf
@@ -1,24 +1,3 @@
-resource "random_pet" "ssh_key_name" {
-  prefix    = "ssh"
-  separator = ""
-}
-
-resource "azapi_resource" "ssh_public_key" {
-  type      = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  name      = random_pet.ssh_key_name.id
-  location  = var.resource_group_location
-  parent_id = var.resource_group_id
-}
-
-resource "azapi_resource_action" "ssh_public_key_gen" {
-  type        = "Microsoft.Compute/sshPublicKeys@2022-11-01"
-  resource_id = azapi_resource.ssh_public_key.id
-  action      = "generateKeyPair"
-  method      = "POST"
-
-  response_export_values = ["publicKey", "privateKey"]
-}
-
 resource "random_pet" "azurerm_kubernetes_cluster_name" {
   prefix = "cluster"
 }
@@ -41,10 +20,6 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     displayName = "Kubernetes Cluster"
   }
 
-  key_vault_secrets_provider {
-    secret_rotation_enabled = false
-  }
-
   identity {
     type = "UserAssigned"
     identity_ids = [
@@ -57,25 +32,13 @@ resource "azurerm_kubernetes_cluster" "k8s" {
     vm_size    = var.vm_size
     node_count = var.node_count
     zones = ["1","2", "3"]
-    type = "VirtualMachineScaleSets"
     vnet_subnet_id = var.vnet_subnet_id
-  }
-
-  linux_profile {
-    admin_username = var.username
-
-    ssh_key {
-      key_data = azapi_resource_action.ssh_public_key_gen.output.publicKey
-    }
   }
 
   network_profile {
     network_plugin    = "azure"
     network_policy  = "azure"
     outbound_type = "userDefinedRouting"
-    load_balancer_sku = "standard"
-    service_cidr = "172.16.0.0/16"
-    dns_service_ip = "172.16.0.10"
   }
 
   ingress_application_gateway {
